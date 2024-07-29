8edd5e7da41a448250878af69211eb9a4ab53ad7
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-07-29 10:01:36 +0000

    Updating to latest FlutterFlow output.

--- lib/flutter_flow/internationalization.dart
+++ lib/flutter_flow/internationalization.dart
@@ -143,7 +143,7 @@ final kTranslationsMap = <Map<String, Map<String, String>>>[
       'en': 'Enter your password...',
       'tl': '',
     },
-    'gqd2aiav': {
+    'zxcc7z6n': {
       'en': '',
       'tl': '',
     },
@@ -317,7 +317,7 @@ final kTranslationsMap = <Map<String, Map<String, String>>>[
       'en': '',
       'tl': '',
     },
-    '6ozgk72u': {
+    '48gg91ry': {
       'en': '',
       'tl': '',
     },
