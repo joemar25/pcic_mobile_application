bc43de4b4906702bce083d88c6f807f0e7bac308
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-08-23 05:30:27 +0000

    Updating to latest FlutterFlow output.

--- lib/flutter_flow/internationalization.dart
+++ lib/flutter_flow/internationalization.dart
@@ -261,7 +261,7 @@ final kTranslationsMap = <Map<String, Map<String, String>>>[
       'en': 'Enter Old Password...',
       'tl': 'Ilagay ang Lumang Password...',
     },
-    'h5rqoiju': {
+    'euwd17ng': {
       'en': '',
       'tl': '',
     },
@@ -273,7 +273,7 @@ final kTranslationsMap = <Map<String, Map<String, String>>>[
       'en': 'Enter New Password...',
       'tl': 'Maglagay ng Bagong Password...',
     },
-    '3xlf5a6b': {
+    '8fxu19fw': {
       'en': '',
       'tl': '',
     },
@@ -285,7 +285,7 @@ final kTranslationsMap = <Map<String, Map<String, String>>>[
       'en': 'Confirm New Password...',
       'tl': 'Kumpirmahin ang bagong password...',
     },
-    '7bk23v96': {
+    'rhucopww': {
       'en': '',
       'tl': '',
     },
