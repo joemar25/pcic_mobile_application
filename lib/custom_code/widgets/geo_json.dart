a4a563dabe4c227ba4c3cb37d6353379f8206f84
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-14 14:54:09 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/widgets/geo_json.dart
+++ lib/custom_code/widgets/geo_json.dart
@@ -5,12 +5,13 @@ import '/backend/sqlite/sqlite_manager.dart';
 import '/actions/actions.dart' as action_blocks;
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
-import '/custom_code/widgets/index.dart'; // Imports other custom widgets
+import 'index.dart'; // Imports other custom widgets
 import '/custom_code/actions/index.dart'; // Imports custom actions
 import '/flutter_flow/custom_functions.dart'; // Imports custom functions
 import 'package:flutter/material.dart';
 // Begin custom widget code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_geojson/flutter_map_geojson.dart';
 import 'package:latlong2/latlong.dart' as latlong2;
