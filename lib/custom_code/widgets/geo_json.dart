897fb397874ae1922d2e547ba2f3ccb0036fab50
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-14 15:21:50 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/widgets/geo_json.dart
+++ lib/custom_code/widgets/geo_json.dart
@@ -12,6 +12,8 @@ import 'package:flutter/material.dart';
 // Begin custom widget code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
+import 'index.dart'; // Imports other custom widgets
+
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_geojson/flutter_map_geojson.dart';
 import 'package:latlong2/latlong.dart' as latlong2;
@@ -51,16 +53,38 @@ class _GeoJsonState extends State<GeoJson> {
 
       print('DEBUG: Parsing GeoJSON data...');
 
-      // Parse the JSON manually to handle type casting
       final jsonData = json.decode(geoJsonString);
       final features = jsonData['features'] as List;
 
       for (var feature in features) {
-        if (feature['geometry']['type'] == 'MultiPolygon') {
-          var coordinates = feature['geometry']['coordinates'] as List;
-          _processMultiPolygon(coordinates);
+        final geometry = feature['geometry'];
+        final properties = feature['properties'];
+
+        switch (geometry['type']) {
+          case 'Point':
+            _processPoint(geometry['coordinates'], properties);
+            break;
+          case 'LineString':
+            _processLineString(geometry['coordinates'], properties);
+            break;
+          case 'Polygon':
+            _processPolygon(geometry['coordinates'], properties);
+            break;
+          case 'MultiPoint':
+            _processMultiPoint(geometry['coordinates'], properties);
+            break;
+          case 'MultiLineString':
+            _processMultiLineString(geometry['coordinates'], properties);
+            break;
+          case 'MultiPolygon':
+            _processMultiPolygon(geometry['coordinates'], properties);
+            break;
+          case 'GeometryCollection':
+            _processGeometryCollection(geometry['geometries'], properties);
+            break;
+          default:
+            print('DEBUG: Unsupported geometry type: ${geometry['type']}');
         }
-        // Add more conditions here for other geometry types if needed
       }
 
       print('DEBUG: GeoJSON data parsed successfully.');
@@ -75,23 +99,98 @@ class _GeoJsonState extends State<GeoJson> {
     }
   }
 
-  void _processMultiPolygon(List coordinates) {
+  void _processPoint(List coordinates, Map<String, dynamic> properties) {
+    double lng = (coordinates[0] is int)
+        ? (coordinates[0] as int).toDouble()
+        : coordinates[0];
+    double lat = (coordinates[1] is int)
+        ? (coordinates[1] as int).toDouble()
+        : coordinates[1];
+    _geoJsonParser.markers.add(Marker(
+      point: latlong2.LatLng(lat, lng),
+      width: 80,
+      height: 80,
+      child: Icon(Icons.location_on, color: Colors.red),
+    ));
+  }
+
+  void _processLineString(List coordinates, Map<String, dynamic> properties) {
+    List<latlong2.LatLng> points = (coordinates as List).map((coord) {
+      double lng = (coord[0] is int) ? (coord[0] as int).toDouble() : coord[0];
+      double lat = (coord[1] is int) ? (coord[1] as int).toDouble() : coord[1];
+      return latlong2.LatLng(lat, lng);
+    }).toList();
+
+    _geoJsonParser.polylines.add(Polyline(
+      points: points,
+      strokeWidth: 2.0,
+      color: Colors.blue,
+    ));
+  }
+
+  void _processPolygon(List coordinates, Map<String, dynamic> properties) {
+    for (var ring in coordinates) {
+      List<latlong2.LatLng> points = (ring as List).map((coord) {
+        double lng =
+            (coord[0] is int) ? (coord[0] as int).toDouble() : coord[0];
+        double lat =
+            (coord[1] is int) ? (coord[1] as int).toDouble() : coord[1];
+        return latlong2.LatLng(lat, lng);
+      }).toList();
+
+      _geoJsonParser.polygons.add(Polygon(
+        points: points,
+        color: Colors.blue.withOpacity(0.3),
+        borderColor: Colors.blue,
+        borderStrokeWidth: 2,
+      ));
+    }
+  }
+
+  void _processMultiPoint(List coordinates, Map<String, dynamic> properties) {
+    for (var point in coordinates) {
+      _processPoint(point, properties);
+    }
+  }
+
+  void _processMultiLineString(
+      List coordinates, Map<String, dynamic> properties) {
+    for (var lineString in coordinates) {
+      _processLineString(lineString, properties);
+    }
+  }
+
+  void _processMultiPolygon(List coordinates, Map<String, dynamic> properties) {
     for (var polygon in coordinates) {
-      for (var ring in polygon) {
-        List<latlong2.LatLng> points = [];
-        for (var coord in ring) {
-          double lat =
-              (coord[1] is int) ? (coord[1] as int).toDouble() : coord[1];
-          double lng =
-              (coord[0] is int) ? (coord[0] as int).toDouble() : coord[0];
-          points.add(latlong2.LatLng(lat, lng));
-        }
-        _geoJsonParser.polygons.add(Polygon(
-          points: points,
-          color: Colors.blue.withOpacity(0.3),
-          borderColor: Colors.blue,
-          borderStrokeWidth: 2,
-        ));
+      _processPolygon(polygon, properties);
+    }
+  }
+
+  void _processGeometryCollection(
+      List geometries, Map<String, dynamic> properties) {
+    for (var geometry in geometries) {
+      switch (geometry['type']) {
+        case 'Point':
+          _processPoint(geometry['coordinates'], properties);
+          break;
+        case 'LineString':
+          _processLineString(geometry['coordinates'], properties);
+          break;
+        case 'Polygon':
+          _processPolygon(geometry['coordinates'], properties);
+          break;
+        case 'MultiPoint':
+          _processMultiPoint(geometry['coordinates'], properties);
+          break;
+        case 'MultiLineString':
+          _processMultiLineString(geometry['coordinates'], properties);
+          break;
+        case 'MultiPolygon':
+          _processMultiPolygon(geometry['coordinates'], properties);
+          break;
+        default:
+          print(
+              'DEBUG: Unsupported geometry type in GeometryCollection: ${geometry['type']}');
       }
     }
   }
@@ -103,14 +202,12 @@ class _GeoJsonState extends State<GeoJson> {
       height: widget.height ?? 400,
       child: FlutterMap(
         options: MapOptions(
-          initialCenter:
-              latlong2.LatLng(12.8797, 121.7740), // Center of Philippines
-          initialZoom: 6, // Adjusted to show more of the Philippines
+          initialCenter: latlong2.LatLng(14.6091, 121.0223),
+          initialZoom: 10,
         ),
         children: [
           TileLayer(
             urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-            userAgentPackageName: 'com.example.app',
           ),
           PolygonLayer(polygons: _geoJsonParser.polygons),
           PolylineLayer(polylines: _geoJsonParser.polylines),
